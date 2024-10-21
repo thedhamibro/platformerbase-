d245f13c7654516b6755753c2d1631f06a13993f
Sehajdeep Singh Dhami <dhamiputtar2009@gmail.com>
2024-10-21 21:28:03 +0530

    update

--- scenes/main menu/main_menu.gd
+++ scenes/main menu/main_menu.gd
@@ -1,20 +1,23 @@
 class_name MainMenu
 extends Control
 
-
 @onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/start_button as Button 
-@onready var exit_buuton: Button = $MarginContainer/HBoxContainer/VBoxContainer/exit_buuton as Button
+@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/exit_buuton as Button
 @export var start_level = preload("res://scenes/main.tscn") as PackedScene
 
+func _ready() -> void:
+	start_button.pressed.connect(self._on_start_pressed)
+	exit_button.pressed.connect(self._on_exit_pressed)
 
-func _ready():
-	start_button.button_down.connect(on_start_pressed)
-	exit_buuton.button_down.connect(on_exit_pressed)
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to(start_level)
+	print("Hello!")
 
-func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(start_level)
-
-
-func on_exit_pressed() -> void:
+func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+	
+	
+	
 	
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,38 +1,72 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+enum State {
+	IDLE,
+	RUN,
+	JUMP,
+	ATTACK
+}
+
+var current_state = State.IDLE
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
+func _ready() -> void:
+	animated_sprite.connect("animation_finished", Callable(self, "_on_animation_finished"))
+
+func _physics_process(delta) -> void:
+	# Apply gravity if not on the floor
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	handle_input()
+	move_and_slide()
+
+func handle_input() -> void:
+	var direction = Input.get_axis("left", "right")
+
+	# Jumping logic
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-
-	var direction = Input.get_axis("left", "right")
-
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction<0:
-		animated_sprite.flip_h = true
-		
-	
-	if direction:
+	# Horizontal movement
+	if direction != 0:
 		velocity.x = direction * SPEED
+		animated_sprite.flip_h = direction < 0
+		set_state(State.RUN)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
+		velocity.x = 0  # Reset horizontal velocity when no input
+		if is_on_floor():
+			set_state(State.IDLE)
 
-	move_and_slide()
+	# Attack logic
+	if is_on_floor() and Input.is_action_just_pressed("attack") and current_state != State.ATTACK:
+		attack()
+
+	# Update state if in the air
+	if not is_on_floor() and current_state != State.JUMP:
+		set_state(State.JUMP)
+
+func attack() -> void:
+	set_state(State.ATTACK)
+	animated_sprite.play("attack1")
+
+func set_state(new_state: State) -> void:
+	if new_state != current_state:
+		current_state = new_state
+		match current_state:
+			State.IDLE:
+				animated_sprite.play("idle")
+			State.RUN:
+				animated_sprite.play("run")
+			State.JUMP:
+				animated_sprite.play("jump")
+			State.ATTACK:
+				animated_sprite.play("attack1")
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "attack1":
+		current_state = State.IDLE if is_on_floor() else State.JUMP
