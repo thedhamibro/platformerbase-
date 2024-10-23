367c67fead36bb6169d6426e58bde205cade7c65
harshG998 <harshmeetsingh797@gmail.com>
2024-10-23 20:16:42 +0530

    ui

--- scenes/main menu/main_menu.gd
+++ scenes/main menu/main_menu.gd
@@ -1,23 +1,15 @@
-class_name MainMenu
 extends Control
 
-@onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/start_button as Button 
-@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/exit_buuton as Button
-@export var start_level = preload("res://scenes/main.tscn") as PackedScene
+func _on_start_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
-func _ready() -> void:
-	start_button.pressed.connect(self._on_start_pressed)
-	exit_button.pressed.connect(self._on_exit_pressed)
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to(start_level)
-	print("Hello!")
 
-func _on_exit_pressed() -> void:
+
+func _on_option_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_quit_pressed() -> void:
 	get_tree().quit()
-
-
-	
-	
-	
-	
