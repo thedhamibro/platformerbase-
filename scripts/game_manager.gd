f7734c0b29e50bd6872ad2901d6299235df8a0c5
harshG998 <harshmeetsingh797@gmail.com>
2024-10-29 19:42:24 +0530

    gg

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,3 +2,9 @@ extends Node
 
 var score = 0
 var coins = 0
+@onready var coinLabel: Label = $Coin
+@onready var scoreLabel: Label = $Score
+
+func _process(delta: float) -> void:
+	$GUI2/Coin.text = str(coins)
+	
